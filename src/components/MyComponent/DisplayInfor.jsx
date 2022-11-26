fb7c9fceea0f823c9a8243af9bc23a601abfea01
ngoanmc <phongnhadn1991@gmail.com>
2022-11-26 21:17:10 +0700

    function Component

--- src/components/MyComponent/DisplayInfor.jsx
+++ src/components/MyComponent/DisplayInfor.jsx
@@ -1,56 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-class DisplayInfor extends React.Component {
-    state = {
-        isShowListUsers: true
+DisplayInfor.propTypes = {
+    
+};
+
+function DisplayInfor(props) {
+    const { listUsers, handleDeleteUser } = props
+    const [isShowListUsers, setisShowListUsers] = useState(true)
+    const handleShowHide = (event) => {
+        setisShowListUsers(!isShowListUsers)
     }
-
-    componentDidMount() {
-        console.log('>>> call me component did mount');
-        setTimeout(() => {
-            document.title = 'MCN Title'
-        },3000)
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshop) {
-        console.log('>>> call me component did update', prevProps , this.props);
-    }
-
-    handleShowHide = (event) => {
-        this.setState({
-            isShowListUsers: !this.state.isShowListUsers
-        })
-    }
-
-    render() {
-        console.log(">>> call me render");
-        // console.log(this.props);
-        const { listUsers, handleDeleteUser } = this.props
-
-        return (
-            <>
-                <div className="btnHandle">
-                    <button onClick={(event) => {this.handleShowHide(event)}}>
-                        {this.state.isShowListUsers === true ? 'Hide' : 'Show'} list users
-                    </button>
+    return (
+        <>
+            <div className="btnHandle">
+                   <button onClick={(event) => {handleShowHide(event)}}>
+                       {isShowListUsers === true ? 'Hide' : 'Show'} list users
+                   </button>
+            </div>
+            { isShowListUsers &&
+                <div className="listUser">
+                    {listUsers.map((user) => {
+                        const { id, name, age } = user
+                        return (
+                            <div key={id} className={+age > 18 ? "green" : "red"}>
+                                My name's {name} <button onClick={() => {handleDeleteUser(id)}}>Delete</button><br/>
+                                My age's {age}
+                                <hr />
+                            </div>
+                        )
+                    })}
                 </div>
-                { this.state.isShowListUsers &&
-                    <div className="listUser">
-                        {listUsers.map((user) => {
-                            const { id, name, age } = user
-                            return (
-                                <div key={id} className={+age > 18 ? "green" : "red"}>
-                                    My name's {name} <button onClick={() => {handleDeleteUser(id)}}>Delete</button><br/>
-                                    My age's {age}
-                                    <hr />
-                                </div>
-                            )
-                        })}
-                    </div>
-                }
-            </>
-        )
-    }
+            }
+        </>
+    );
 }
 
 export default DisplayInfor
