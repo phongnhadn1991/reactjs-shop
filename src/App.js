c5979fb4f1f6f32e71e9fb409372d851a479e6d5
ngoanmc <phongnhadn1991@gmail.com>
2022-11-27 01:48:49 +0700

    routes react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.scss';
 import Header from './components/Header/Header';
+import { Outlet } from "react-router-dom";
+
 const App = () => {
   return (
     <div className="app-container">
-      <Header />
+      <div className='p-header'>
+        <Header />
+      </div>
+      <div className='main-content'>
+      <div className='p-sidenav'></div>
+      <div className='app-content'>
+        <Outlet />
+      </div>
+      </div>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,23 +3,25 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
+import { NavLink } from "react-router-dom";
+
 const Header = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">MCN</Navbar.Brand>
+        <NavLink to='/' className='navbar-brand'>MCN</NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">User</Nav.Link>
-            <Nav.Link href="#link">Admin</Nav.Link>
+            <NavLink to='/' className='nav-link'>Home</NavLink>
+            <NavLink to='/users' className='nav-link'>User</NavLink>
+            <NavLink to='/admins' className='nav-link'>Admin</NavLink>
           </Nav>
           <Nav>
           <NavDropdown title="Setting" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Logout</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
+            <NavDropdown.Item>Login</NavDropdown.Item>
+                <NavDropdown.Item>Logout</NavDropdown.Item>
+                <NavDropdown.Item>Profile</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
